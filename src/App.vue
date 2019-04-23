4346110e860db0d58ad128fa226f267cbbf77b5e
ieber <i.e.berdnikov@yandex.ru>
2019-04-23 15:45:44 +0300

    pubpanel

--- src/App.vue
+++ src/App.vue
@@ -8,21 +8,7 @@
               Вспомогательная информация
             </v-card-title>
             <v-card-text style="max-height:424px">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac luctus purus, vitae egestas nunc. Donec posuere a lacus vel placerat. Aenean sit amet eros eu lectus suscipit bibendum quis a leo. Duis massa enim, accumsan et nisi eu, laoreet suscipit metus. Integer mattis felis id enim rhoncus, quis vulputate dui laoreet. Morbi erat turpis, semper eu urna in, cursus porta elit. Nulla urna odio, scelerisque vitae tempor ullamcorper, venenatis ut arcu. Donec eu nulla in justo rhoncus egestas vitae eget sapien. Curabitur vel elit at metus iaculis consectetur quis sed massa. Morbi vitae molestie ipsum. Quisque nec lacus nec erat rutrum dictum. Morbi cursus in magna non tempus.
-              </p>
-              <p>
-                Etiam convallis eu diam vitae egestas. Aliquam vitae magna ut massa lacinia cursus. Proin malesuada augue nec fringilla lacinia. Sed at sem nec ante mattis imperdiet. Nulla euismod cursus nibh, quis pellentesque magna gravida vestibulum. Aliquam ornare iaculis tortor, vulputate eleifend enim. Nulla facilisi. Nam ac justo risus.
-              </p>
-              <p>
-                Aliquam urna felis, interdum id justo non, sagittis dictum felis. Aliquam quis imperdiet velit. Cras tincidunt elit felis. Integer ex quam, maximus scelerisque lobortis cursus, sodales at libero. Phasellus semper, erat sit amet tristique iaculis, libero mi finibus lorem, vel eleifend urna lorem in ligula. Nunc in ligula vel orci venenatis accumsan. Mauris ultrices elit rutrum luctus tempor. Vivamus non arcu mauris. Suspendisse dapibus ipsum odio, ac maximus dui mattis quis. Nam ultricies at risus et euismod. Suspendisse scelerisque et orci vel suscipit. Phasellus dictum nulla id libero eleifend eleifend. Nunc faucibus eleifend fringilla. Sed laoreet malesuada sem in mattis. Nam hendrerit, lorem nec rhoncus auctor, lectus eros mollis libero, quis sollicitudin urna tortor non ipsum.
-              </p>
-              <p>
-                Integer vel imperdiet lorem. Suspendisse lobortis sapien sed viverra malesuada. Mauris eget nisi non felis accumsan tristique scelerisque eget nulla. Nulla pellentesque sollicitudin enim, sit amet imperdiet mauris elementum quis. Nullam in ipsum libero. Nulla facilisi. Etiam laoreet blandit ligula sed sagittis. Nam diam eros, bibendum at turpis sed, euismod accumsan lectus.
-              </p>
-              <p>
-                Donec hendrerit nec eros scelerisque sodales. Vivamus eu massa et neque aliquam interdum. Vestibulum ac vehicula dui. Aliquam nec gravida ligula. Etiam molestie est purus, ut tincidunt nisi fermentum nec. Ut auctor odio eu nibh euismod, sit amet volutpat mauris lacinia. Vivamus sit amet semper dolor, non iaculis magna. Sed pharetra purus diam, sed viverra metus maximus id. Vestibulum condimentum felis sed faucibus blandit. Etiam tempor lacus neque, eget tincidunt neque sollicitudin id.
-              </p>
+
             </v-card-text>
           </v-card>
         </v-dialog>
