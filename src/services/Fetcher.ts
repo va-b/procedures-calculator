9c82d5008857f2d8bc47709f231157ac9a19beb9
ieber <i.e.berdnikov@yandex.ru>
2019-06-18 23:57:33 +0300

    hotfix

--- src/services/Fetcher.ts
+++ src/services/Fetcher.ts
@@ -14,7 +14,7 @@ export default class Fetcher implements IHttpClient
         };
     }
     
-    private dataparser(val: any, key: string): any
+    private dataparser(key: string, val: any): any
     {
         let type = typeof (val);
         let dateRegExp: RegExp = /\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z|)/;
